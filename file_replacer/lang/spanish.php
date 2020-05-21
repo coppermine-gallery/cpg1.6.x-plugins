bf1c092f56715f08cc3272278cc1240270a75dd3
heismauri <47569205+heismauri@users.noreply.github.com>
2020-05-21 14:11:52 -0400

    Translation to spanish

---
+++ file_replacer/lang/spanish.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('IN_COPPERMINE')) {
+    die('Not in Coppermine...');
+}
+
+$lang_plugin_file_replacer['replace_file'] = 'Remplazar archivo';
+$lang_plugin_file_replacer['file_replacer'] = 'Remplazador de archivos';
+$lang_plugin_file_replacer['upload_file'] = 'Subir archivo que remplazará al existente';
+$lang_plugin_file_replacer['browse'] = 'Buscar';
+$lang_plugin_file_replacer['upload'] = 'Subir';
+$lang_plugin_file_replacer['description'] = 'Permite que los archivos sean remplazados por otro archivo';
+$lang_plugin_file_replacer['announcement_thread'] = 'Announcement thread';
+$lang_plugin_file_replacer['author'] = 'Creado por %s para cpg1.4.x - adaptado para cpg1.5.x & cpg1.6.x e internalización por %s';
+$lang_plugin_file_replacer['update_timestamp'] = 'Actualizar timestamp';
+$lang_plugin_file_replacer['error_move_file'] = 'Hubo un error moviendo el archivo \'%s\' a su nuevo destino \'%s\'';
+$lang_plugin_file_replacer['overwrite_metadata'] = 'Sobrescribir el título, descripción y palabras claves existentes con los medatados del archivo';
